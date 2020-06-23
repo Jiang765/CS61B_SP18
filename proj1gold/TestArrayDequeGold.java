0cea7eccb42fce12a008d86c084dbdb7df37e513
Jiang765 <Lola.Jiang@outlook.com>
2020-06-23 11:45:25 +0800

    Update proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -12,37 +12,37 @@ public class TestArrayDequeGold {
     public void testArray() {
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        String message = "";
 
         for (int i = 0; i < 1000; i += 1) {
             int number = StdRandom.uniform(1000);
             sad.addLast(number);
             ads.addLast(number);
-            for (int j = 0; j < ads.size() - 1; j++) {
-                assertEquals("addLast is incorrect！Expected: " + ads.get(j)
-                        + " Actual: " + sad.get(j), ads.get(j), sad.get(j));
-            }
+            message += "addLast(" + number + ")\n";
+            assertEquals(message, ads.get(i), sad.get(i));
+
         }
 
         for (int i = 0; i < 1000; i += 1) {
             int number = StdRandom.uniform(1000);
             sad.addFirst(number);
             ads.addFirst(number);
-            for (int j = 0; j < ads.size() - 1; j++) {
-                assertEquals("addFirst is incorrect！Expected: " + ads.get(j)
-                        + " Actual: " + sad.get(j), ads.get(j), sad.get(j));
-            }
+            message += "addFirst(" + number + ")\n";
+            assertEquals(message, ads.get(0), sad.get(0));
         }
 
         for (int i = 0; i < 1000; i += 1) {
             Integer r1 = ads.removeFirst();
             Integer r2 = sad.removeFirst();
-            assertEquals("removeFirst is incorrect！Expected: " + r1 + " Actual: " + r2, r1, r2);
+            message += "removeLast()\n";
+            assertEquals(message, r1, r2);
         }
 
         for (int i = 0; i < 1000; i += 1) {
             Integer r1 = ads.removeLast();
             Integer r2 = sad.removeLast();
-            assertEquals("removeLast is incorrect！Expected: " + r1 + " Actual: " + r2, r1, r2);
+            message += "removeFirst()\n";
+            assertEquals(message, r1, r2);
         }
     }
 }
